1deeab7df656031d8b98e170339f20909506af7b
Giorgiocirrone <cirronegiorgio@gmail.com>
2024-08-19 15:40:10 +0200

    fine primo esercizio

--- js/script.js
+++ js/script.js
@@ -33,3 +33,63 @@ Buon lavoro e buon divertimento! :slightly_smiling_face:
 
 
 */
+
+/*
+ESERCIZIO 1 - Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampiamo in pagina i due tiri e il risultato
+
+*/
+
+
+
+
+
+//il massimo che io posso ottenere è 4..99 quinid arrotondo per eccesso  e aggiungo fino a dove posso arrivare 
+//Math.floor(Math.random()*X)+1 dove x  è il numero che voglio ottenere
+
+
+
+//Fase di preparazione delle variabili 
+const maxNumber = 6
+const randomCPUNumber = Math.floor(Math.random() * maxNumber) + 1;
+
+/*validazione */
+console.log(randomCPUNumber);
+
+const userNumber = parseInt(prompt(`Scegli un nuemmro da 1 a ${maxNumber}`));
+
+
+
+
+if (userNumber > 6) {
+
+    window.alert('hai sbagliato sei uscito fuori dal range di valori');
+}
+
+
+if (userNumber === randomCPUNumber) {
+
+    console.log('Nessuno dei due ha vinto');
+}
+
+
+if (userNumber > randomCPUNumber) {
+
+    console.log(`Hai vinto!`);
+} else {
+
+    console.log(`Hai perso!`);
+}
+
+
+
+
+
+
+
+
+
+
+
